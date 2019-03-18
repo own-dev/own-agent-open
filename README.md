0c34bcbc2f32777feee7b3caf3df8c6de76935e3
Aleksandr Nedorezov <Al73rus@gmail.com>
2019-03-18 13:17:03 +0300

    Updated deployment instructions and README

--- README.md
+++ README.md
@@ -2,3 +2,45 @@
 
 This a Hello world Agent that is meant to be used as a template in the OWN.space hackathon.
 You can enhance it to implement your own functionality.
+
+# Clone the source code
+`git config user.name "Your Name"`
+`git config user.email your@mail.com`
+
+`cd` to the directory you want to clone own-agent code
+`git clone https://github.com/own-dev/own-agent`
+
+# Add necessary environment variables to .bashrc
+! Note: if you are deploying the system for development and/or testing purposes,
+please use test account credentials for all the agents
+```
+export OWN_AGENTS_PATH="/opt/own-agent" # where opt is the directory you cloned own-agent code to
+export PYTHONPATH="$OWN_AGENTS_PATH:$OWN_AGENTS_PATH/agents_platform:$OWN_AGENTS_PATH/agents:$OWN_AGENTS_PATH/agents/news:$OWN_AGENTS_PATH/agents/ip:$OWN_AGENTS_PATH/agents/science:$OWN_AGENTS_PATH/agents_platform/own_adapter"
+export OWN_AGENT_ADDRESS="http://alpha.own.space/" # Change to http://0.0.0.0:9002/ to test with local backend
+export OWN_PLATFORM_PREFIX=""
+export OWN_PLATFORM_PROTOCOL="ws"
+export OWN_TEST_LOGIN="{SPECIFY_LOGIN_FROM_ALPHA.OWN.SPACE}"
+export OWN_TEST_PASSWORD="{SPECIFY_PASSWORD_FROM_ALPHA.OWN.SPACE}"
+export OWN_TEST_AGENT_LOGIN="{SPECIFY_LOGIN_FROM_ALPHA.OWN.SPACE}"
+export OWN_TEST_AGENT_PASSWORD="{SPECIFY_LOGIN_FROM_ALPHA.OWN.SPACE}"
+export REDIS_ADDRESS="127.0.0.1"
+export REDIS_PORT="6379"
+export USE_LOCAL_IP="True"
+```
+
+Authenticate to google cloud from your local machine:
+https://googleapis.github.io/google-cloud-python/latest/core/auth.html
+
+# Activate virtual environment
+
+`source $OWN_AGENTS_PATH/ownenv/bin/activate`
+
+# Deploy
+`./own-agent/deployment.sh`
+
+# Run
+1. `cd own-agent`
+2. Start all the agents in background
+    1. by script, running `screen python3 agents_starter.py`
+    2. manually, like `nohup python3 agents/{agent_name}/api.py &`
+3. Start the agents platform engine (`python3 agents_platform/engine.py &`)
--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,55 @@
+#!/usr/bin/env bash
+
+# apt installation
+sudo apt-get install -y python-virtualenv
+sudo apt-get install -y python-pip
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y python-dev
+sudo apt-get install -y build-essential
+sudo apt-get install -y libpq-dev
+sudo apt-get install -y python-tk
+sudo apt-get install -y redis-server
+sudo apt install python3-distutils
+# libs for matplotlib
+sudo apt-get install -y libfreetype6-dev
+sudo apt-get install -y libpng12-dev
+# uncomment the next string if you want to use redis-cli
+# sudo apt-get install -y redis-tools
+sudo apt-get install -y python3-tk
+# GCC is required for building the wordcloud module
+sudo apt-get install -y gcc
+# Required for chromium in funding agent
+sudo apt-get install libasound2 libasound2-plugins libnss3-dev
+# To install Twisted
+sudo apt install python3.6-dev build-essential libssl-dev libffi-dev
+
+# locale to eliminate problems with installing virtual environment
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+sudo dpkg-reconfigure locales
+
+# install virtual environment
+virtualenv -p python3 ownenv
+
 # pip requirements
+source ownenv/bin/activate
 sudo pip install --upgrade setuptools
-pip install -r pip-requirements.txt
+pip install -r own-agent/pip-requirements.txt
+
+# if there are problems with installing the Pillow or wordcloud modules, please refer to
+# https://stackoverflow.com/questions/34631806/fail-during-installation-of-pillow-python-module-in-linux
+# also can try to run "sudo apt-get install python3-dev"
+
+echo "add next lines to your ~/.bashrc file"
+echo "export OWN_AGENTS_PATH=\"/srv/own-agent\" # where srv is the directory you cloned own-agent code to"
+echo "export PYTHONPATH=\"$OWN_AGENTS_PATH:$OWN_AGENTS_PATH/agents_platform:$OWN_AGENTS_PATH/agents:$OWN_AGENTS_PATH/agents/news:$OWN_AGENTS_PATH/agents/ip:$OWN_AGENTS_PATH/agents/science:$OWN_AGENTS_PATH/agents_platform/own_adapter\""
+echo "export OWN_AGENT_ADDRESS=\"http://alpha.own.space\" # Change to http://0.0.0.0:9002/ to test with local backend
+echo "export OWN_PLATFORM_PREFIX=\"\""
+echo "export OWN_PLATFORM_PROTOCOL=\"ws\""
+echo "export OWN_TEST_LOGIN=\"{SPECIFY_LOGIN_FROM_ALPHA.OWN.SPACE}\""
+echo "export OWN_TEST_PASSWORD=\"{SPECIFY_PASSWORD_FROM_ALPHA.OWN.SPACE}\""
+echo "export OWN_TEST_LOGIN=\"{SPECIFY_LOGIN_FROM_ALPHA.OWN.SPACE}\""
+echo "export OWN_TEST_PASSWORD=\"{SPECIFY_PASSWORD_FROM_ALPHA.OWN.SPACE}\""
+echo "export REDIS_ADDRESS=\"127.0.0.1\""
+echo "export REDIS_PORT=\"6379\""
+echo "export USE_LOCAL_IP=\"True\""
